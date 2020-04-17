3fc4b084310f04a256a888b457ef2ea41f449f99
BehnamEbrahimi <behnam.ebrahimi@gmail.com>
2020-04-17 20:45:32 +1000

    First Commit

---
+++ Application/Dtos/ModelDto.cs
@@ -0,0 +1,8 @@
+namespace Application.Dtos
+{
+    public class ModelDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Persistence/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Make> Makes { get; set; }
+        public DbSet<Model> Models { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
